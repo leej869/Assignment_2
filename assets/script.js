adba5490e19b7fb84f16cf8b39d7c0259692d8b3
Juna Lee <leej869@newschool.edu>
2019-10-01 17:39:18 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,17 @@
 
 
-var j = 0; // to keep track of how many times you want to loop, you have to simulate a for loop:
-
 var timer = 100;
+
+
+
 
 var myLoopFunction = function(i){
 
 	var rotation = Math.round((i/10) * 360);	
 	var positionLeft = Math.floor(Math.random()*document.body.clientWidth);
 	var positionRight = Math.floor(Math.random()*document.body.clientHeight);
-	var changingColor = (217 - (i/2))
-	var changingColorTwo = (144 - (i/5))
+	var changingColor = (217 - (i))
+	var changingColorTwo = (144 - (i/2))
 
 	document.getElementById("root_6_settimeout").insertAdjacentHTML("beforeend", `
 		<div class="square" style="transform:rotate(${rotation}deg); position:relative; left:${positionLeft}px; top:${positionRight}px;border-color: rgb(255,${changingColor},${changingColorTwo});"></div>
@@ -19,7 +20,16 @@ var myLoopFunction = function(i){
 
 for (var i = 0; i < 100; i++) {
 	setTimeout(myLoopFunction, i*100, i)
+
 }
+
+	var myButton = document.getElementsByClassName("square")
+
+	myButton[0].addEventListener("mouseover", function(){
+	document.getElementsByClassName("square").remove();
+})
+
+
 
 
 
