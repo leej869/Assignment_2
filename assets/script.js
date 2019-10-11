af2cb0a834f910c519544966a6c167778ce05d98
Juna Lee <leej869@newschool.edu>
2019-10-10 20:42:24 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,29 @@
 
 
-var timer = 100;
+  var timer = 100;
 
 
 
 
-var myLoopFunction = function(i){
+  var myLoopFunction = function(i){
 
-	var rotation = Math.round((i/10) * 360);	
-	var positionLeft = Math.floor(Math.random()*document.body.clientWidth);
-	var positionRight = Math.floor(Math.random()*document.body.clientHeight);
-	var changingColor = (217 - (i))
-	var changingColorTwo = (144 - (i/2))
+    var rotation = Math.round((i/10) * 360);
+    var positionLeft = Math.floor(Math.random()*document.body.clientWidth);
+    var positionRight = Math.floor(Math.random()*document.body.clientHeight);
+    var changingColor = (217 - (i))
+    var changingColorTwo = (144 - (i/2))
 
-	document.getElementById("root_6_settimeout").insertAdjacentHTML("beforeend", `
-		<div class="square" style="transform:rotate(${rotation}deg); position:relative; left:${positionLeft}px; top:${positionRight}px;border-color: rgb(255,${changingColor},${changingColorTwo});"></div>
-	 `);
-}
+    document.getElementById("root_6_settimeout").insertAdjacentHTML("beforeend", `<div class="square" id="square`+i+`" onmouseover="mouseover(`+i+`);" style="transform:rotate(${rotation}deg); left:${positionLeft}px; top:${positionRight}px;border-color: rgb(255,${changingColor},${changingColorTwo});"></div>`);}
 
-for (var i = 0; i < 100; i++) {
-	setTimeout(myLoopFunction, i*100, i)
+  for (var i = 0; i < 100; i++) {
+    setTimeout(myLoopFunction, i*100, i)
 
-}
+  }
 
-	var myButton = document.getElementsByClassName("square")
+  var myButton = document.getElementsByClassName("square");
 
-	myButton[0].addEventListener("mouseover", function(){
-	document.getElementsByClassName("square").remove();
-})
+  function mouseover(i) {
+    document.getElementById("square"+i).style.display = "none";
 
-
-
-
-
+    setTimeout(myLoopFunction, 100, Math.round(Math.random()*200))
+  }
